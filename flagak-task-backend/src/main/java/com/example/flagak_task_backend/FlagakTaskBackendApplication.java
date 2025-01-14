57b85d2cf467a7b67a450f50f77c3ad6281fc5fd
Mohamed Mourad <mohamed.m_2000@outlook.com>
2025-01-14 14:26:46 +0200

    backend starter
    generated backend starter code using Spring Initializr with the required dependencies.

---
+++ flagak-task-backend/src/main/java/com/example/flagak_task_backend/FlagakTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.flagak_task_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagakTaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlagakTaskBackendApplication.class, args);
+	}
+
+}
---
+++ flagak-task-backend/src/test/java/com/example/flagak_task_backend/FlagakTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flagak_task_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagakTaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
